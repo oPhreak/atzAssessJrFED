1588e53606ec1876e1bb3f78960daab6c938d5e0
eyang <hs.eric.yang@gmail.com>
2022-01-25 16:13:11 -0800

    Added image resource, product obj, started fleshing out Panel component

---
+++ src/products.js
@@ -0,0 +1,46 @@
+const products = [
+    {
+        id: 1,
+        img: "/products/Image 1.jpg",
+        brand: "Wilfred Free",
+        name: "Chicago",
+        price: "$79"
+    },
+    {
+        id: 2,
+        img: "/products/Image 2.jpg",
+        brand: "Babaton",
+        name: "St. Louis",
+        price: "$119"
+    },
+    {
+        id: 3,
+        img: "/products/Image 1.jpg",
+        brand: "Wilfred",
+        name: "Seattle",
+        price: "$79"
+    },
+    {
+        id: 4,
+        img: "/products/Image 1.jpg",
+        brand: "Wilfred",
+        name: "Austin",
+        price: "$89"
+    },
+    {
+        id: 5,
+        img: "/products/Image 1.jpg",
+        brand: "Babaton",
+        name: "Boston",
+        price: "$119"
+    },
+    {
+        id: 6,
+        img: "/products/Image 1.jpg",
+        brand: "Wilfred Free",
+        name: "Denver",
+        price: "$99"
+    }
+];
+
+export default products;
