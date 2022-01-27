6a8e0aed834f055347d37c2f87d4be1965f9c498
eyang <hs.eric.yang@gmail.com>
2022-01-26 22:20:24 -0800

    successfully implemented mui/Grid

--- src/components/RenderPanels.jsx
+++ src/components/RenderPanels.jsx
@@ -5,25 +5,19 @@ import Panel from "./Panel";
 function RenderPanels(props) {
     const panels = props.products;
 
-    const displayPanels = panels.map((panel, index) => {
-        return (
+
+    return(
+        <Grid container spacing={{ xs: 2, md: 3 }} columns ={{ xs: 4, sm: 6, md: 12}}>
+            {Array.from(panels).map((panel, index) => (
                 <Grid item xs={2} sm={4} md={4} key={index}>
                     <Panel
-                        id = {index}
                         img = {panel.img}
                         brand = {panel.brand}
                         name = {panel.name}
                         price = {panel.price}
                     />
                 </Grid>
-        );
-        });
-
-    return(
-        <Grid container spacing={{ xs: 2, md: 3 }} columns ={{ xs: 4, sm: 6, md: 12}}>
-            <div>
-                {displayPanels}
-            </div>
+            ))}
         </Grid>
     );
 }
